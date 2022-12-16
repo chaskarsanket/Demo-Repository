874a44a07c126d1b61b21ce6cf4580a5eb2ee692
chaskarsanket <120700335+chaskarsanket@users.noreply.github.com>
2022-12-16 10:28:04 +0530

    BasicTest
    Attempted first Git commit

---
+++ sample_project/src/basic/TestBase.java
@@ -0,0 +1,13 @@
+package basic;
+
+public class TestBase {
+	
+	public static void main(String args[])
+	{
+		System.out.println("Hello Worold");
+		
+		System.out.println("Testbase");
+		System.out.println("Added dependecnies in pom.xml");
+	}
+
+}
